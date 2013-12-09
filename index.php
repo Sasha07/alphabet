bc25ce74e8b5ec163b14368cabd73eccc403ec23
Sasha Andreev <aaa.sonic@mail.ru>
2013-12-09 18:11:23 +0400

    init project

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$loader = new Twig_Loader_String();
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('Hello {{ name }}!', array('name' => 'Fabien'));
