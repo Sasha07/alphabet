f3bbfe77b8ba8327b00bc8162d701eb2cf20acb0
Sasha Andreev <aaa.sonic@mail.ru>
2013-12-11 22:29:32 +0400

    Изменена структура на "Carousel"

---
+++ templates_cache/62/f4/bc438ec76020e7d6e88dc4affd7aa047fb5487095c727dc70ea91b039167.php
@@ -0,0 +1,56 @@
+<?php
+
+/* second.twig */
+class __TwigTemplate_62f4bc438ec76020e7d6e88dc4affd7aa047fb5487095c727dc70ea91b039167 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = $this->env->loadTemplate("base.twig");
+
+        $this->blocks = array(
+            'pageTitle' => array($this, 'block_pageTitle'),
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_pageTitle($context, array $blocks = array())
+    {
+        echo "Главная страница";
+    }
+
+    // line 5
+    public function block_content($context, array $blocks = array())
+    {
+        // line 6
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "second.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  38 => 6,  35 => 5,  29 => 3,);
+    }
+}
--- templates_cache/ba/42/3df494ba733d124e439eda25752441153e9fe47b2484c0d6694649e5dab7.php
+++ templates_cache/ba/42/3df494ba733d124e439eda25752441153e9fe47b2484c0d6694649e5dab7.php
@@ -18,66 +18,96 @@ class __TwigTemplate_ba423df494ba733d124e439eda25752441153e9fe47b2484c0d6694649e
     protected function doDisplay(array $context, array $blocks = array())
     {
         // line 1
-        echo "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\"
-        \"http://www.w3.org/TR/html4/loose.dtd\">
+        echo "<!DOCTYPE html>
 <html>
 <head>
 
     <title>";
-        // line 6
+        // line 5
         $this->displayBlock('pageTitle', $context, $blocks);
         echo " | Азбука грамотности</title>
 
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
     <meta charset=\"utf-8\" />
 
     <link href=\"vendor/twitter/bootstrap/dist/css/bootstrap.css\" rel=\"stylesheet\" />
     <link href=\"vendor/twitter/bootstrap/dist/css/bootstrap-theme.css\" rel=\"stylesheet\" />
     <link href=\"css/main.css\" rel=\"stylesheet\" />
+    <link href=\"css/carousel.css\" rel=\"stylesheet\" />
 
+    <script src=\"js/jquery.js\"></script>
     <script src=\"js/html5shiv.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/dist/js/bootstrap.js\"></script>
+
+    <!-- Bootstrap JQuery Scripts -->
+    <script src=\"vendor/twitter/bootstrap/js/affix.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/js/alert.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/js/button.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/js/carousel.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/js/collapse.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/js/dropdown.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/js/modal.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/js/popover.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/js/scrollspy.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/js/tab.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/js/tooltip.js\"></script>
+    <script src=\"vendor/twitter/bootstrap/js/transition.js\"></script>
 
 </head>
 <body>
 
-<header>
-    <nav class=\"navbar navbar-default\" role=\"navigation\" id=\"mainmenu\">
 
-        <!-- Главная кнопка-название -->
-        <div class=\"navbar-header\">
-            <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\"></button>
-            <a class=\"navbar-brand\" href=\"#\">Азбука грамотности</a>
-        </div>
 
-        <!-- Меню -->
-        <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
-            <ul class=\"nav navbar-nav\">
-                <li ";
-        // line 31
+<div class=\"container-narrow\">
+
+    <div class=\"navbar-wrapper\">
+        <div class=\"container\">
+
+            <div class=\"navbar navbar-inverse navbar-static-top\" role=\"navigation\">
+                <div class=\"container\">
+                    <div class=\"navbar-header\">
+                        <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\".navbar-collapse\">
+                            <span class=\"sr-only\">Toggle navigation</span>
+                            <span class=\"icon-bar\"></span>
+                            <span class=\"icon-bar\"></span>
+                            <span class=\"icon-bar\"></span>
+                        </button>
+                        <a class=\"navbar-brand\" href=\"/\">Азбука грамотности</a>
+                    </div>
+
+                    <div class=\"navbar-collapse collapse\">
+                        <ul class=\"nav navbar-nav\">
+                            <li ";
+        // line 57
         if (((isset($context["currentPage"]) ? $context["currentPage"] : null) == "main")) {
             echo "class=\"active\"";
         }
         echo "><a href=\"/\">Главная</a></li>
-                <li><a href=\"#\">Страница №2</a></li>
-                <li><a href=\"#\">Страница №3</a></li>
-                <li><a href=\"#\">Страница №4</a></li>
-            </ul>
+                            <li><a href=\"second\">Страница №2</a></li>
+                            <li><a href=\"#\">Страница №3</a></li>
+                            <li><a href=\"#\">Страница №4</a></li>
+                        </ul>
+                    </div>
 
-            <!-- Пример формы в заголовке
-            <form class=\"navbar-form navbar-left\" role=\"search\">
-                <div class=\"form-group\">
+                    <!-- Пример формы в заголовке
+                    <form class=\"navbar-form navbar-left\" role=\"search\">
+                    <div class=\"form-group\">
                     <input type=\"text\" class=\"form-control\" placeholder=\"Search\">
+                    </div>
+                    <button type=\"submit\" class=\"btn btn-default\">Submit</button>
+                    </form>-->
+
                 </div>
-                <button type=\"submit\" class=\"btn btn-default\">Submit</button>
-            </form>
-            -->
+            </div>
+
         </div>
-    </nav>
-</header>
+    </div>
+
 
 ";
-        // line 49
+        // line 79
         $this->displayBlock('content', $context, $blocks);
-        // line 50
+        // line 80
         echo "
 <footer>
     <hr>
@@ -85,16 +115,18 @@ class __TwigTemplate_ba423df494ba733d124e439eda25752441153e9fe47b2484c0d6694649e
     <a href=\"/\" id=\"footerlink\">На главную</a>
 </footer>
 
+</div>
+
 </body>
 </html>";
     }
 
-    // line 6
+    // line 5
     public function block_pageTitle($context, array $blocks = array())
     {
     }
 
-    // line 49
+    // line 79
     public function block_content($context, array $blocks = array())
     {
     }
@@ -111,6 +143,6 @@ class __TwigTemplate_ba423df494ba733d124e439eda25752441153e9fe47b2484c0d6694649e
 
     public function getDebugInfo()
     {
-        return array (  98 => 49,  93 => 6,  81 => 50,  79 => 49,  56 => 31,  28 => 6,  21 => 1,);
+        return array (  130 => 79,  125 => 5,  111 => 80,  109 => 79,  82 => 57,  27 => 5,  21 => 1,);
     }
 }
