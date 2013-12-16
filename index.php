557cd1eb5edaf41a3e61b345bdd1d0511d78365c
Andrey Andreev <aandreev@evelopers.com>
2013-12-16 16:53:33 +0400

    При входе в режим администрирования остаёмся на той же странице

--- index.php
+++ index.php
@@ -30,13 +30,13 @@ if(isset($_GET['url'])) {
 
         if(!isset($_SERVER['PHP_AUTH_USER'])) {
             auth();
-                    } else {
+        } else {
             if((md5($_SERVER['PHP_AUTH_USER']) === '21232f297a57a5a743894a0e4a801fc3' && md5($_SERVER['PHP_AUTH_PW']) === 'bd9570a3fd2d6cf4ca7532def9e1c88b') || $_SESSION['isAuth']) {
                 if(!$_SESSION['isAuth']) {
                     $_SESSION['isAuth'] = true;
                     $_SESSION['KCFINDER']['disabled'] = false;
                     $_SESSION['ip'] = $_SERVER["SERVER_ADDR"];
-                    header('location: /');
+                    header('location: '.(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '/'));
                     exit;
                 }
             }
